1988999c14eb8d4c21b16160dbc5c89ce62f5246
javidancode <javidanig@code.edu.az>
2022-07-04 04:11:41 +0400

    Added Extentions Pow table

---
+++ Extention Pow/Helpers/Extentions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extention_Pow.Helpers
+{
+    public static class Extentions
+    {
+        public static double  Power( this int  num1,int pow1)
+        {
+            return Math.Pow(num1 , pow1 );
+        }
+    }
+}
---
+++ Extention Pow/Program.cs
@@ -0,0 +1,17 @@
+﻿using Extention_Pow.Helpers;
+using System;
+
+namespace Extention_Pow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            num1.Power(5);
+            int pow1 = 5;
+
+            Console.WriteLine(num1.Power(pow1));
+        }
+    }
+}
